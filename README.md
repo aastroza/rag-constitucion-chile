1f6678ddec8cb8ccd8b8d93e3455a5de9e3ea545
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2023-07-27 18:22:57 -0400

    streamlit cloud

---
+++ README.md
@@ -0,0 +1,25 @@
+# RAG-Constitucion-Chile
+
+## Getting Started
+
+Before you begin to run this project, there are a few prerequisites you will need to have in place:
+
+✅ **OpenAI API Key:** In order to interact with OpenAI's API, you will need to have an API key. You can obtain this by creating an account on OpenAI's website and following their [instructions to generate an API key](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key).
+
+✅ **Modal Account:** The application is containerized for deployment using the Modal platform. Visit the [Modal](https://modal.com/signup) website to sign up for an account if you don't have one already.
+
+✅ **Streamlit Account (Optional):** While you can run Streamlit apps locally without an account, having a Streamlit account allows you to deploy and share your apps, which can be useful for demonstrating your project to others. If you wish to use this feature, [sign up for a Streamlit account](https://share.streamlit.io/signup).
+
+## Installation
+
+```
+conda create --name rag-constitucion-chile -c conda-forge python=3.10
+conda activate rag-constitucion-chile
+pip install -r requirements.txt
+```
+
+## [CLIENT] Streamlit app
+
+```
+streamlit run st_app.py
+```
---
+++ st_app.py
@@ -0,0 +1,38 @@
+# Import the required libraries
+import streamlit as st
+from citation_engine import query_engine_vigente, query_engine_propuesta, get_final_response
+
+#st.set_page_config(layout="wide")
+
+# Set the title of the Streamlit application
+st.title("Compara las Constituciones de Chile")
+prompt = st.text_input("Ingresa un tema de tu interés, te contaremos que dice cada Constitución al respecto, luego te comentaremos sus similitudes y diferencias.", value='Las atribuciones del Banco Central')
+
+if st.button("Consultar"):
+
+    with st.spinner('Espera unos segundos, estamos procesando tu consulta...'):
+        response_vigente = query_engine_vigente.query(prompt)
+        response_propuesta = query_engine_propuesta.query(prompt)
+        response_final = get_final_response(prompt, response_vigente, response_propuesta)
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.subheader("Constitución Actual")
+            st.markdown(response_vigente)
+            st.subheader("Fuentes")
+            for node in response_vigente.source_nodes:
+                [source, capitulo, articulo] = node.node.get_text().split('\n', 3)[0:3]
+                st.markdown(f'-{source.replace("Source", "Fuente" )}, {capitulo}, {articulo}\n')
+
+        with col2:
+            st.subheader("Constitución Propuesta")
+            st.markdown(response_propuesta)
+            st.subheader("Fuentes")
+            for node in response_propuesta.source_nodes:
+                [source, capitulo, articulo] = node.node.get_text().split('\n', 3)[0:3]
+                st.markdown(f'-{source.replace("Source", "Fuente" )}, {capitulo}, {articulo}\n')
+
+        with col3:
+            st.subheader("Comparación")
+            st.markdown(response_final)
